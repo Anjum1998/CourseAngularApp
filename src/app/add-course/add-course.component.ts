594e5bee89a782fe86b776b3256aa909bda44147
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-11-28 11:48:41 +0530

    update

--- src/app/add-course/add-course.component.ts
+++ src/app/add-course/add-course.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-course.component.css']
 })
 export class AddCourseComponent {
+  courseTitle=""
+  courseDescription=""
+  courseDate=""
+  courseDuration=""
+  courseVenue=""
 
 }
